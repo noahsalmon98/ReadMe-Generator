1df1740979314d86cda02b26238c49d93e1c5ee3
Noah Salmon <noahsalmon98@gmail.com>
2023-08-30 13:13:47 -0400

    added questions to the index.js

--- Develope/index.js
+++ Develope/index.js
@@ -6,7 +6,79 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of the project?",
+    },
+    {
+        type: "input",
+        message: "Please enter a brief description of the project.",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "Please provide further details about your project and its general purpose.",
+        name: "about",
+    },
+    {
+        type: "input",
+        message: "What are the installations used for your project?",
+        name: "installation",
+    },
+
+    {
+        type: "list",
+        name: "license",
+        message: "Please select the license used for this project:",
+        choices: [
+            "Apache License 2.0",
+            "GNU General Public License v3.0",
+            "MIT License",
+            'BSD 2-Clause "Simplified" License',
+            'BSD 3-Clause "New" or "Revised" License',
+            "Boost Software License 1.0",
+            "Creative Commons Zero v1.0 Universal",
+            "Eclipse Public License 2.0",
+            "GNU Affero General Public License v3.0",
+            "GNU Lesser General Public License v2.1",
+            "Mozilla Public License 2.0",
+            "The Unlicense",
+            "None",
+        ],
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please enter any testing protocols used for your project.",
+    },
+    {
+        type: "input",
+        name: "author",
+        message: "What is your name?",
+    },
+    {
+        type: "input",
+        name: "userName",
+        message: "What is your GitHub username?",
+    },
+    {
+        type: "input",
+        name: "userEmail",
+        message: "What is your email?",
+    },
+    {
+        type: "input",
+        name: "URL",
+        message: "What is the URL of the deployed site?",
+    },
+    {
+        type: "input",
+        name: "repo",
+        message: "What is the URL of the GitHub Repo?",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
