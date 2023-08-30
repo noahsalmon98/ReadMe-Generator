bb6b8e5f669fe59c6a1e66497ce5220af8e4ed98
Noah Salmon <noahsalmon98@gmail.com>
2023-08-30 13:40:34 -0400

    added licenses to generateMarkdown.js

--- Develope/utils/generateMarkdown.js
+++ Develope/utils/generateMarkdown.js
@@ -1,6 +1,19 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (license) {
+        const licenseBadges = {
+          'MIT': "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+          "Apache 2.0":
+            "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+          "GNU GPLv3":
+            "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+          'ISC': "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)",
+        };
+        return licenseBadges[license];
+      }
+      return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
