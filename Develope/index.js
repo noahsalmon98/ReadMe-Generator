cb167aae04d2271288a73edd96a510070c85f14d
Noah Salmon <noahsalmon98@gmail.com>
2023-08-29 21:17:34 -0400

    double checking commit

--- Develope/index.js
+++ Develope/index.js
@@ -1,4 +1,9 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
