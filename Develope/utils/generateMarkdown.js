76615ff9e874e92d63bbe313fc9c08d0f2447108
Noah Salmon <noahsalmon98@gmail.com>
2023-08-30 13:47:02 -0400

    added function to return license links and added function to return license section of readme

--- Develope/utils/generateMarkdown.js
+++ Develope/utils/generateMarkdown.js
@@ -16,12 +16,26 @@ function renderLicenseBadge(license) {
 }
 
 // TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    function renderLicenseLink(license) {
+        if (license) {
+          const licenseLinks = {
+            'MIT': "https://opensource.org/licenses/MIT",
+            "Apache 2.0": "https://opensource.org/licenses/Apache-2.0",
+            "GNU GPLv3": "https://www.gnu.org/licenses/gpl-3.0",
+            'ISC': "https://opensource.org/licenses/ISC",
+          };
+          return `[License]( ${licenseLinks[license]} )`;
+        }
+        return "";
+      }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    return license == 'None' ? '' : `${data.title} has a ${license} license.`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
